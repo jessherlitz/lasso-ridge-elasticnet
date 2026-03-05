1ec8d0209bf334582f1a4504b49de24f8732c9d3
jessherlitz <jesssherlitz@gmail.com>
2026-03-05 11:02:39 -0500

    stepwise, lasso and elastic

--- homework_8.R
+++ homework_8.R
@@ -2,7 +2,7 @@
 
 library(glmnet)
 
-## Question 11.1.1
+## Question 11.1.1--------------------------------------------------------------
 
 # loading data
 crime_data <- read.table(
@@ -12,26 +12,45 @@ crime_data <- read.table(
 
 #View(crime_data)
 
+## simple regression
 regression_model <- lm(Crime ~ ., data = crime_data)
 summary(regression_model)
 
+## AIC stepwise
 stepwise_regression_model <- step(regression_model, 
                                   direction = "both", 
                                   trace = TRUE)
-
 summary(stepwise_regression_model)
 
+## BIC stepwise
 stepwise_bic <- step(regression_model, 
                      direction = "both", 
                      k = log(nrow(crime_data)),
                      trace = TRUE)
 summary(stepwise_bic)
 
+# testing new data
+test_data <- data.frame(
+  M = 14.0, So = 0, Ed = 10.0, Po1 = 12.0, Po2 = 15.5,
+  LF = 0.640, M.F = 94.0, Pop = 150, NW = 1.1, U1 = 0.120,
+  U2 = 3.6, Wealth = 3200, Ineq = 20.1, Prob = 0.04, Time = 39.0
+)
 
-## Question 11.1.2
+# AIC prediction
+aic_prediction <- predict(stepwise_regression_model, newdata = test_data)
+print(aic_prediction)
+
+# BIC prediction
+bic_prediction <- predict(stepwise_bic, newdata = test_data)
+print(bic_prediction)
+
+## Question 11.1.2--------------------------------------------------------------
 
 scaled_predictors <- as.matrix(scale(crime_data[, 1:15]))
+scaled_predictors
+
 response <- crime_data[, 16]
+response
 
 set.seed(111)
 lasso_cross_validation <- cv.glmnet(scaled_predictors, response, alpha = 1)
@@ -50,7 +69,8 @@ cbind(lambda = lasso_cross_validation$lambda,
 colMeans(crime_data[, 1:15])
 apply(crime_data[, 1:15], 2, sd)
 
-## Question 11.1.3
+
+## Question 11.1.3--------------------------------------------------------------
 
 alphas <- seq(0, 1, 0.1)
 print(alphas)
@@ -59,12 +79,12 @@ set.seed(111)
 cv_errors <- c()
 
 for (a in alphas) {
-  model <- cv.glmnet(
+  elastic_net_model <- cv.glmnet(
     scaled_predictors, 
     response, 
     alpha = a
   )
-  cv_errors <- c(cv_errors, min(model$cvm))
+  cv_errors <- c(cv_errors, min(elastic_net_model$cvm))
 }
 print(cv_errors)
 
@@ -73,20 +93,34 @@ cbind(alpha = alphas, minimum_cv_error = cv_errors)
 best_alpha <- alphas[which.min(cv_errors)]
 print(best_alpha)
 
+#plot
+plot(alphas, cv_errors, 
+     type = "b",
+     xlab = "Alpha", 
+     ylab = "Minimum CV Error",
+     main = "Elastic Net - CV Error vs Alpha",
+     pch = 19)
+abline(v = best_alpha, lty = 2)
+
+## cross validation
 set.seed(111)
-best_model <- cv.glmnet(
+elastic_net_best_model <- cv.glmnet(
   scaled_predictors, 
   response, 
   alpha = best_alpha
 )
 
-coef(best_model, s = "lambda.min")
-coef(best_model, s = "lambda.1se")
-best_model$lambda.min
-best_model$lambda.1se
+coef(elastic_net_best_model, s = "lambda.min")
+coef(elastic_net_best_model, s = "lambda.1se")
+elastic_net_best_model$lambda.min
+elastic_net_best_model$lambda.1se
+
+cbind(lambda = elastic_net_best_model$lambda,
+      mean_error = elastic_net_best_model$cvm,
+      std_error = elastic_net_best_model$cvsd)
 
 ### plotting
-plot(best_model)
+plot(elastic_net_best_model)
 
 # testing new data
 test_data <- data.frame(
@@ -101,11 +135,13 @@ scaled_test <- scale(test_data,
 
 scaled_test <- as.matrix(scaled_test)
 
-## predicting using our test data
-predict(best_model, s = best_model$lambda.min, newx = scaled_test)
+## predicting using our test data using Elastic Net
+predict(best_model, s = elastic_net_best_model$lambda.min, newx = scaled_test)
+predict(best_model, s = elastic_net_best_model$lambda.1se, newx = scaled_test)
+
+## predicting using our test data using Lasso model
 predict(lasso_cross_validation, s = lasso_cross_validation$lambda.min, newx = scaled_test)
-
-
+predict(lasso_cross_validation, s = lasso_cross_validation$lambda.1se, newx = scaled_test)
 
 
 
